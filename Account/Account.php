f43229dffa89562d9b32fc546bf2a124b4e3702c
root <josipperkovic13@gmail.com>
2017-01-14 20:20:31 +0100

    new

--- Account/Account.php
+++ Account/Account.php
@@ -1,26 +1,13 @@
 <?php
-
-
 namespace Account;
-
-
 use Data\DataStructure;
-
 class Account
 {
     private $balance;
     private $transactions;
-
     public function __construct(\Balance\Balance $balance = null)
     {
         $this->balance=$balance;
         $this->transactions=new Transactions();
-
     }
-
-
-
-
-
-
 }
