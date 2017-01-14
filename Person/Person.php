07be110f0ff191d70af0165dfcb5d8a1717ee4aa
root <josipperkovic13@gmail.com>
2017-01-14 20:32:59 +0100

    new with class Person

---
+++ Person/Person.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Person;
+
+use Data\DataStructureReaderInterface;
+
+class Person
+{
+    private $dataStructure;
+
+
+    public function __constructor(DataStructureReaderInterface $dataStructure = null)
+    {
+
+        $this->dataStructure = dataStructure;
+
+    }
+
+    public function setDataStructure(DataStructureReaderInterface $dataStructure)
+    {
+            $this->dataStructure = $dataStructure;
+    }
+
+    public function name() : string
+    {
+             return $this->dataStructure->getValue('person_name');
+    }
+}
